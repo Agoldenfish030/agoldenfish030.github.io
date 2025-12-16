362213044423b40e7dd316a912b4345fd0b6480c
Agoldenfish030 <rain20854@gmail.com>
2025-12-16 14:16:19 +0800

    save the change of href

--- tester/loginTest.js
+++ tester/loginTest.js
@@ -1,24 +1,22 @@
+const crypto = require("crypto");
+
 function checkButton(){
     document.getElementById("signIn").addEventListener("click", authorizationStart, false);
 }
 
 function authorizationStart(){
-    /*
-    https://auth.atlassian.com/authorize?
-    audience=api.atlassian.com&
-    client_id=OCRrG6JXdgcJ9hZaFINjXXYcUzm8mqh0&
-    scope=read:confluence-space.summary%20
-        write:confluence-space%20
-        write:confluence-file%20
-        read:confluence-props%20
-        write:confluence-props%20
-        read:confluence-content.all%20
-        read:confluence-content.summary&
-    redirect_uri=https://YOUR_APP_CALLBACK_URL&
-    state=YOUR_USER_BOUND_VALUE&
-    response_type=code&
-    prompt=consent
-    */
+    window.location.href("https://auth.atlassian.com/authorize?audience=api.atlassian.com& \
+                        client_id=OCRrG6JXdgcJ9hZaFINjXXYcUzm8mqh0& \
+                        scope=read%3Aconfluence-space.summary%20 \
+                            write%3Aconfluence-space%20 \
+                            write%3Aconfluence-file%20 \
+                            read%3Aconfluence-props%20 \
+                            write%3Aconfluence-props%20 \
+                            read%3Aconfluence-content.all%20 \
+                            read%3Aconfluence-content.summary& \
+                        redirect_uri=https%3A%2F%2Fagoldenfish030.github.io%2FTooMuchStonesToDo%2FIndex.html& \
+                        state=${YOUR_USER_BOUND_VALUE}& \
+                        response_type=code&prompt=consent");
 }
 
 window.addEventListener("load", checkButton, false);
