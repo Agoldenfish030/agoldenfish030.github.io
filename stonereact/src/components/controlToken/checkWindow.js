374b46b1a8ec0fa9119338a26fa78be4319158c6
Agoldenfish030 <rain20854@gmail.com>
2025-12-21 20:30:20 +0800

    test update and update checkWindow

--- stonereact/src/components/controlToken/checkWindow.js
+++ stonereact/src/components/controlToken/checkWindow.js
@@ -1,11 +1,3 @@
-import { useNavigate } from 'react-router-dom';
-const navigate = useNavigate();
-window.addEventListener('load', ()=>{
-    if(!sessionStorage.length){
-        navigate('https://too-much-stone-to-do.vercel.app');
-    }
-});
-
 window.addEventListener('beforeunload', async()=>{
     const state = sessionStorage.getItem('userState');
     const response = await fetch('https://toomuchstonestodo.onrender.com/userToken', {
